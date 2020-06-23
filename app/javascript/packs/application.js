91740599274fdae1b0ea80e38c97af203a6dace3
Given <givenyslilm12@gmail.com>
2020-06-23 17:44:37 +0200

    Styled the show page:

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -14,6 +14,7 @@ require('bootstrap')
 require('owl.carousel')
 require('jquery-touchswipe')
 require('./bs.touch.swipe')
+window.Swiper = require('swiper')
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
